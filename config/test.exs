6988a78fc44d046799c1ba7037fd259ed6ef9c24
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2021-01-19 10:08:50 -0300

    changed test config file to use other config in github actions

--- config/test.exs
+++ config/test.exs
@@ -14,6 +14,13 @@ config :nfl_rushing, NflRushing.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :app, App.Repo,
+    username: "postgres",
+    password: "postgres"
+end
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :nfl_rushing, NflRushingWeb.Endpoint,
